cac6dc7321592976ab956bb5d356f163becfe366
Charly Mourglia <charly.mourglia@diginext.fr>
2018-10-01 15:52:45 +0200

    Updated build scripts

--- Install/doBuild.bat
+++ Install/doBuild.bat
@@ -1,7 +1,8 @@
 @echo off
 call "%~dp0setEnvironment.bat"
 
-cmake --build %THRIFT_GENERATED%
+cmake --build %THRIFT_GENERATED% -- /nologo /v:q
+cmake --build %THRIFT_GENERATED% --target INSTALL -- /nologo /v:q
 REM cmake --build %THRIFT_GENERATED% --config Build -- /nologo /v:q
 
 pause
