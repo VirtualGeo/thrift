cc0218b02efa21ca06694ceac5652c348efdbf62
Charly Mourglia <charly.mourglia@diginext.fr>
2018-10-23 17:51:36 +0200

    [WIP] C# Generation

--- compiler/cpp/src/thrift/generate/t_c_api_generator.cc
+++ compiler/cpp/src/thrift/generate/t_c_api_generator.cc
@@ -37,6 +37,7 @@ public:
     out_dir_base_ = "gen-c_api";
 
     nspace_ = program->get_namespace("cpp");
+    cs_nspace_ = program->get_namespace("csharp");
 
     if (!nspace_.empty()) {
       nspace_ = dots_to_underscore(nspace_);
@@ -69,6 +70,8 @@ private:
   string nspace_uc_;
   string nspace_lc_;
 
+  string cs_nspace_;
+
   string define_name_;
 
   // Formatting only variables;
@@ -81,7 +84,7 @@ private:
   int source_indent_ = 0;
   int csharp_indent_ = 0;
 
-  set<std::string> csharp_keywords_;
+  set<std::string> cs_keywords_;
 
 private:
   std::ostream& make_doc(std::ostream& stream, t_doc* doc, bool add_lf = false);
@@ -109,10 +112,12 @@ private:
   void convert_c_type_to_cpp(t_type* type, string c_name, string cpp_name);
   void convert_cpp_type_to_c(t_type* type, string cpp_name, string c_name);
   void convert_cs_type_to_c(t_type* type, string cs_name, string c_name);
+  void convert_c_type_to_cs(t_type* type, string c_name, string cs_name);
 
   string get_c_type_name(t_type* type) const;
   string get_cpp_type_name(t_type* type) const;
   string get_cs_type_name(t_type* type) const;
+  string get_cs_to_c_type_name(t_type* type, bool is_param = false) const;
 
   string get_c_struct_name(t_type* type) const;
   string get_cpp_struct_name(t_type* type) const;
@@ -124,6 +129,8 @@ private:
 
   string get_c_function_signature(t_function* tfunction, string prefix, bool dllexport);
   string get_init_service_name(t_service* tservice) const;
+
+  string get_cs_dll_import(t_service* tservice) const;
 
   void push_indent(int indent);
   void pop_indent(int* indent);
@@ -204,7 +211,16 @@ void t_c_api_generator::init_source() {
 void t_c_api_generator::init_csharp() {
   string name = get_out_dir() + program_name_ + "_api.cs";
   f_csharp.open(name.c_str());
-  f_csharp << autogen_comment();
+  f_csharp << autogen_comment() << endl;
+
+  f_csharp << "using System.Runtime.InteropServices;" << endl << endl;
+
+  if (!cs_nspace_.empty()) {
+    f_csharp << "namespace " << cs_nspace_ << endl;
+    push_indent(csharp_indent_);
+    open_scope(f_csharp);
+    pop_indent(&csharp_indent_);
+  }
 
   init_csharp_keywords();
 }
@@ -227,6 +243,11 @@ void t_c_api_generator::close_source() {
 }
 
 void t_c_api_generator::close_csharp() {
+  if (!cs_nspace_.empty()) {
+    push_indent(csharp_indent_);
+    close_scope(f_csharp);
+    pop_indent(&csharp_indent_);
+  }
   f_csharp.close();
 }
 
@@ -411,7 +432,140 @@ void t_c_api_generator::generate_service_source(t_service* tservice) {
 }
 
 void t_c_api_generator::generate_service_csharp(t_service* tservice) {
+  const string c_service_name = nspace_lc_ + camel_case_to_underscores(tservice->get_name()) + "_";
+
   push_indent(csharp_indent_);
+  indent(f_csharp) << "namespace " << tservice->get_name() << endl;
+  open_scope(f_csharp);
+
+  indent(f_csharp) << "public class DirectClient" << endl;
+  open_scope(f_csharp);
+
+  auto enums = tservice->get_program()->get_enums();
+  for (size_t i = 0; i < enums.size(); ++i) {
+    auto tenum = enums[i];
+    indent(f_csharp) << "private enum " << get_c_type_name(tenum) << endl;
+    open_scope(f_csharp);
+    for (auto value : tenum->get_constants()) {
+      indent(f_csharp) << get_c_enum_value_name(tenum, value) << " = " << value->get_value() << ","
+                       << endl;
+    }
+    close_scope(f_csharp) << ";" << endl << endl;
+  }
+
+  auto structs = tservice->get_program()->get_structs();
+  for (size_t i = 0; i < structs.size(); ++i) {
+    auto tstruct = structs[i];
+    indent(f_csharp) << "[StructLayout(LayoutKind.Sequential)]" << endl;
+    indent(f_csharp) << "private struct " << get_c_struct_name(tstruct) << endl;
+    open_scope(f_csharp);
+    for (auto member : tstruct->get_sorted_members()) {
+      indent(f_csharp) << "public " << get_cs_to_c_type_name(member->get_type()) << " "
+                       << member->get_name() << ";" << endl;
+    }
+    close_scope(f_csharp) << ";" << endl << endl;
+  }
+
+  // Init functions
+  indent(f_csharp) << get_cs_dll_import(tservice) << endl;
+  indent(f_csharp) << "private static extern void init_service_" << get_init_service_name(tservice)
+                   << ";" << endl
+                   << endl;
+
+  indent(f_csharp) << get_cs_dll_import(tservice) << endl;
+  indent(f_csharp) << "private static extern void close_service_" << get_init_service_name(tservice)
+                   << ";" << endl
+                   << endl;
+
+  auto functions = tservice->get_functions();
+  for (auto function : functions) {
+    indent(f_csharp) << get_cs_dll_import(tservice) << endl;
+    indent(f_csharp) << "private static extern ";
+
+    t_type* return_type = function->get_returntype();
+
+    if (!return_type) {
+      f_csharp << "void";
+    } else {
+      f_csharp << get_cs_to_c_type_name(return_type);
+    }
+
+    f_csharp << " " << c_service_name << camel_case_to_underscores(function->get_name()) << "(";
+    auto args = function->get_arglist()->get_sorted_members();
+    for (size_t j = 0; j < args.size(); ++j) {
+      auto arg = args[j];
+      if (j != 0) {
+        f_csharp << ", ";
+      }
+      f_csharp << get_cs_to_c_type_name(arg->get_type(), true) << " " << arg->get_name();
+    }
+    f_csharp << ");" << endl << endl;
+  }
+
+  // Ctor / Dtor
+  indent(f_csharp) << "public DirectClient()" << endl;
+  open_scope(f_csharp);
+  indent(f_csharp) << "init_service_" << get_init_service_name(tservice) << ";" << endl;
+  close_scope(f_csharp) << endl << endl;
+
+  indent(f_csharp) << "~DirectClient()" << endl;
+  open_scope(f_csharp);
+  indent(f_csharp) << "close_service_" << get_init_service_name(tservice) << ";" << endl;
+  close_scope(f_csharp) << endl << endl;
+
+  for (size_t i = 0; i < functions.size(); ++i) {
+    auto function = functions[i];
+    string function_name = function->get_name();
+    if (cs_keywords_.find(function_name) != cs_keywords_.end()) {
+      function_name = "@" + function_name;
+    }
+
+    indent(f_csharp) << "public " << get_cs_type_name(function->get_returntype()) << " "
+                     << function_name << "(";
+
+    auto args = function->get_arglist()->get_sorted_members();
+    for (size_t j = 0; j < args.size(); ++j) {
+      if (j != 0) {
+        f_csharp << ", ";
+      }
+
+      auto arg = args[j];
+      f_csharp << get_cs_type_name(arg->get_type()) << " " << arg->get_name();
+    }
+
+    f_csharp << ")" << endl;
+    open_scope(f_csharp);
+
+    for (auto arg : args) {
+      const string cs_name = arg->get_name();
+      const string c_name = "c_" + arg->get_name();
+
+      indent(f_csharp) << get_cs_to_c_type_name(arg->get_type()) << " " << c_name << ";" << endl;
+
+      convert_cs_type_to_c(arg->get_type(), cs_name, c_name);
+
+      f_csharp << endl;
+    }
+
+    indent(f_csharp) << c_service_name << camel_case_to_underscores(function->get_name()) << "(";
+    for (size_t j = 0; j < args.size(); ++j) {
+      if (j != 0) {
+        f_csharp << ", ";
+      }
+
+      f_csharp << "c_" << args[j]->get_name();
+    }
+    f_csharp << ");" << endl;
+
+    close_scope(f_csharp) << endl;
+
+    if (i != functions.size() - 1) {
+      f_csharp << endl;
+    }
+  }
+
+  close_scope(f_csharp) << endl;
+  close_scope(f_csharp) << endl;
   pop_indent(&csharp_indent_);
 }
 
@@ -493,6 +647,11 @@ string t_c_api_generator::get_c_function_signature(t_function* tfunction,
 
 string t_c_api_generator::get_init_service_name(t_service* tservice) const {
   return nspace_lc_ + camel_case_to_underscores(tservice->get_name()) + "()";
+}
+
+string t_c_api_generator::get_cs_dll_import(t_service* tservice) const {
+  return "[DllImport(\"" + tservice->get_name()
+         + ".dll\", CallingConvention = CallingConvention.Cdecl)]";
 }
 
 void t_c_api_generator::convert_c_type_to_cpp(t_type* type, string c_name, string cpp_name) {
@@ -591,7 +750,54 @@ void t_c_api_generator::convert_cpp_type_to_c(t_type* type, string cpp_name, str
   f_source << endl;
 }
 
-void t_c_api_generator::convert_cs_type_to_c(t_type* type, string cs_name, string c_name) {}
+void t_c_api_generator::convert_cs_type_to_c(t_type* type, string cs_name, string c_name) {
+  type = get_underlying_type(type);
+
+  if (is_base_type(type)) {
+    indent(f_csharp) << c_name << " = " << cs_name << ";" << endl;
+  } else if (type->is_enum()) {
+    t_enum* tenum = (t_enum*)type;
+    auto consts = tenum->get_constants();
+
+    // Prevent cs compiler from complaining
+    indent(f_csharp) << c_name << " = " << get_c_struct_name(tenum) << "."
+                     << get_c_enum_value_name(tenum, consts[0]) << ";" << endl;
+
+    indent(f_csharp) << "switch (" << cs_name << ") {" << endl;
+    indent_up();
+    for (auto value : consts) {
+      indent(f_csharp) << "case " << get_cs_enum_value_name(tenum, value) << ":" << endl;
+      indent_up();
+      indent(f_csharp) << c_name << " = " << get_c_struct_name(tenum) << "."
+                       << get_c_enum_value_name(tenum, value) << ";" << endl;
+      indent(f_csharp) << "break;" << endl;
+      indent_down();
+    }
+    indent_down();
+    indent(f_csharp) << "}";
+    f_csharp << endl;
+  } else if (type->is_container()) {
+    assert(false);
+  } else if (type->is_struct()) {
+    for (auto arg : ((t_struct*)type)->get_sorted_members()) {
+      string cs_arg_name = arg->get_name();
+      cs_arg_name[0] = ::toupper(cs_arg_name[0]);
+
+      const string cs_param_name = cs_name + "." + cs_arg_name;
+      const string c_param_name = c_name + "." + arg->get_name();
+
+      convert_cs_type_to_c(arg->get_type(), cs_param_name, c_param_name);
+    }
+    f_csharp << endl;
+  } else {
+    std::cerr << get_c_type_name(type) << endl;
+    assert(false);
+  }
+}
+
+void t_c_api_generator::convert_c_type_to_cs(t_type* type, string c_name, string cs_name) {
+  indent(f_csharp) << endl;
+}
 
 void t_c_api_generator::push_indent(int indent) {
   previous_indent_ = indent_count();
@@ -716,8 +922,39 @@ string t_c_api_generator::get_cpp_type_name(t_type* type) const {
 }
 
 string t_c_api_generator::get_cs_type_name(t_type* type) const {
-  std::cerr << "get_cs_type_name() is not implemented" << std::endl;
-  return "";
+  type = get_underlying_type(type);
+  if (is_base_type(type)) {
+    return get_cs_to_c_type_name(type, false);
+  }
+
+  return type->get_name();
+}
+
+string t_c_api_generator::get_cs_to_c_type_name(t_type* type, bool is_param) const {
+  type = get_underlying_type(type);
+  if (is_base_type(type)) {
+    t_base_type* base_type = (t_base_type*)type;
+    switch (base_type->get_base()) {
+    case t_base_type::TYPE_VOID:
+      return "void";
+    case t_base_type::TYPE_BOOL:
+      return "bool";
+    case t_base_type::TYPE_I8:
+      return "sbyte";
+    case t_base_type::TYPE_I16:
+      return "short";
+    case t_base_type::TYPE_I32:
+      return "int";
+    case t_base_type::TYPE_I64:
+      return "long";
+    case t_base_type::TYPE_DOUBLE:
+      return "double";
+    case t_base_type::TYPE_STRING:
+      return is_param ? "[MarshalAs(UnmanagedType.LPStr)] string" : "string";
+    }
+  }
+
+  return get_c_type_name(type);
 }
 
 string t_c_api_generator::get_c_struct_name(t_type* type) const {
@@ -738,126 +975,126 @@ string t_c_api_generator::get_c_enum_value_name(t_enum* tenum, t_enum_value* tva
 }
 
 string t_c_api_generator::get_cpp_enum_value_name(t_enum* tenum, t_enum_value* tvalue) const {
-  return tenum->get_name() + "::" + tvalue->get_name();
+  return nspace_ + "::" + tenum->get_name() + "::" + tvalue->get_name();
 }
 
 string t_c_api_generator::get_cs_enum_value_name(t_enum* tenum, t_enum_value* tvalue) const {
-  std::cerr << "get_cs_value_name() is not implemented" << std::endl;
-  return string();
+  return tenum->get_name() + "." + tvalue->get_name();
 }
 
 void t_c_api_generator::init_csharp_keywords() {
-  csharp_keywords_.clear();
+  cs_keywords_.clear();
 
   // C# keywords
-  csharp_keywords_.insert("abstract");
-  csharp_keywords_.insert("as");
-  csharp_keywords_.insert("base");
-  csharp_keywords_.insert("bool");
-  csharp_keywords_.insert("break");
-  csharp_keywords_.insert("byte");
-  csharp_keywords_.insert("case");
-  csharp_keywords_.insert("catch");
-  csharp_keywords_.insert("char");
-  csharp_keywords_.insert("checked");
-  csharp_keywords_.insert("class");
-  csharp_keywords_.insert("const");
-  csharp_keywords_.insert("continue");
-  csharp_keywords_.insert("decimal");
-  csharp_keywords_.insert("default");
-  csharp_keywords_.insert("delegate");
-  csharp_keywords_.insert("do");
-  csharp_keywords_.insert("double");
-  csharp_keywords_.insert("else");
-  csharp_keywords_.insert("enum");
-  csharp_keywords_.insert("event");
-  csharp_keywords_.insert("explicit");
-  csharp_keywords_.insert("extern");
-  csharp_keywords_.insert("false");
-  csharp_keywords_.insert("finally");
-  csharp_keywords_.insert("fixed");
-  csharp_keywords_.insert("float");
-  csharp_keywords_.insert("for");
-  csharp_keywords_.insert("foreach");
-  csharp_keywords_.insert("goto");
-  csharp_keywords_.insert("if");
-  csharp_keywords_.insert("implicit");
-  csharp_keywords_.insert("in");
-  csharp_keywords_.insert("int");
-  csharp_keywords_.insert("interface");
-  csharp_keywords_.insert("internal");
-  csharp_keywords_.insert("is");
-  csharp_keywords_.insert("lock");
-  csharp_keywords_.insert("long");
-  csharp_keywords_.insert("namespace");
-  csharp_keywords_.insert("new");
-  csharp_keywords_.insert("null");
-  csharp_keywords_.insert("object");
-  csharp_keywords_.insert("operator");
-  csharp_keywords_.insert("out");
-  csharp_keywords_.insert("override");
-  csharp_keywords_.insert("params");
-  csharp_keywords_.insert("private");
-  csharp_keywords_.insert("protected");
-  csharp_keywords_.insert("public");
-  csharp_keywords_.insert("readonly");
-  csharp_keywords_.insert("ref");
-  csharp_keywords_.insert("return");
-  csharp_keywords_.insert("sbyte");
-  csharp_keywords_.insert("sealed");
-  csharp_keywords_.insert("short");
-  csharp_keywords_.insert("sizeof");
-  csharp_keywords_.insert("stackalloc");
-  csharp_keywords_.insert("static");
-  csharp_keywords_.insert("string");
-  csharp_keywords_.insert("struct");
-  csharp_keywords_.insert("switch");
-  csharp_keywords_.insert("this");
-  csharp_keywords_.insert("throw");
-  csharp_keywords_.insert("true");
-  csharp_keywords_.insert("try");
-  csharp_keywords_.insert("typeof");
-  csharp_keywords_.insert("uint");
-  csharp_keywords_.insert("ulong");
-  csharp_keywords_.insert("unchecked");
-  csharp_keywords_.insert("unsafe");
-  csharp_keywords_.insert("ushort");
-  csharp_keywords_.insert("using");
-  csharp_keywords_.insert("virtual");
-  csharp_keywords_.insert("void");
-  csharp_keywords_.insert("volatile");
-  csharp_keywords_.insert("while");
+  cs_keywords_.insert("abstract");
+  cs_keywords_.insert("as");
+  cs_keywords_.insert("base");
+  cs_keywords_.insert("bool");
+  cs_keywords_.insert("break");
+  cs_keywords_.insert("byte");
+  cs_keywords_.insert("case");
+  cs_keywords_.insert("catch");
+  cs_keywords_.insert("char");
+  cs_keywords_.insert("checked");
+  cs_keywords_.insert("class");
+  cs_keywords_.insert("const");
+  cs_keywords_.insert("continue");
+  cs_keywords_.insert("decimal");
+  cs_keywords_.insert("default");
+  cs_keywords_.insert("delegate");
+  cs_keywords_.insert("do");
+  cs_keywords_.insert("double");
+  cs_keywords_.insert("else");
+  cs_keywords_.insert("enum");
+  cs_keywords_.insert("event");
+  cs_keywords_.insert("explicit");
+  cs_keywords_.insert("extern");
+  cs_keywords_.insert("false");
+  cs_keywords_.insert("finally");
+  cs_keywords_.insert("fixed");
+  cs_keywords_.insert("float");
+  cs_keywords_.insert("for");
+  cs_keywords_.insert("foreach");
+  cs_keywords_.insert("goto");
+  cs_keywords_.insert("if");
+  cs_keywords_.insert("implicit");
+  cs_keywords_.insert("in");
+  cs_keywords_.insert("int");
+  cs_keywords_.insert("interface");
+  cs_keywords_.insert("internal");
+  cs_keywords_.insert("is");
+  cs_keywords_.insert("lock");
+  cs_keywords_.insert("long");
+  cs_keywords_.insert("namespace");
+  cs_keywords_.insert("new");
+  cs_keywords_.insert("null");
+  cs_keywords_.insert("object");
+  cs_keywords_.insert("operator");
+  cs_keywords_.insert("out");
+  cs_keywords_.insert("override");
+  cs_keywords_.insert("params");
+  cs_keywords_.insert("private");
+  cs_keywords_.insert("protected");
+  cs_keywords_.insert("public");
+  cs_keywords_.insert("readonly");
+  cs_keywords_.insert("ref");
+  cs_keywords_.insert("return");
+  cs_keywords_.insert("sbyte");
+  cs_keywords_.insert("sealed");
+  cs_keywords_.insert("short");
+  cs_keywords_.insert("sizeof");
+  cs_keywords_.insert("stackalloc");
+  cs_keywords_.insert("static");
+  cs_keywords_.insert("string");
+  cs_keywords_.insert("struct");
+  cs_keywords_.insert("switch");
+  cs_keywords_.insert("this");
+  cs_keywords_.insert("throw");
+  cs_keywords_.insert("true");
+  cs_keywords_.insert("try");
+  cs_keywords_.insert("typeof");
+  cs_keywords_.insert("uint");
+  cs_keywords_.insert("ulong");
+  cs_keywords_.insert("unchecked");
+  cs_keywords_.insert("unsafe");
+  cs_keywords_.insert("ushort");
+  cs_keywords_.insert("using");
+  cs_keywords_.insert("virtual");
+  cs_keywords_.insert("void");
+  cs_keywords_.insert("volatile");
+  cs_keywords_.insert("while");
 
   // C# contextual keywords
-  csharp_keywords_.insert("add");
-  csharp_keywords_.insert("alias");
-  csharp_keywords_.insert("ascending");
-  csharp_keywords_.insert("async");
-  csharp_keywords_.insert("await");
-  csharp_keywords_.insert("descending");
-  csharp_keywords_.insert("dynamic");
-  csharp_keywords_.insert("from");
-  csharp_keywords_.insert("get");
-  csharp_keywords_.insert("global");
-  csharp_keywords_.insert("group");
-  csharp_keywords_.insert("into");
-  csharp_keywords_.insert("join");
-  csharp_keywords_.insert("let");
-  csharp_keywords_.insert("orderby");
-  csharp_keywords_.insert("partial");
-  csharp_keywords_.insert("remove");
-  csharp_keywords_.insert("select");
-  csharp_keywords_.insert("set");
-  csharp_keywords_.insert("value");
-  csharp_keywords_.insert("var");
-  csharp_keywords_.insert("where");
-  csharp_keywords_.insert("yield");
+  cs_keywords_.insert("add");
+  cs_keywords_.insert("alias");
+  cs_keywords_.insert("ascending");
+  cs_keywords_.insert("async");
+  cs_keywords_.insert("await");
+  cs_keywords_.insert("descending");
+  cs_keywords_.insert("dynamic");
+  cs_keywords_.insert("from");
+  cs_keywords_.insert("get");
+  cs_keywords_.insert("global");
+  cs_keywords_.insert("group");
+  cs_keywords_.insert("into");
+  cs_keywords_.insert("join");
+  cs_keywords_.insert("let");
+  cs_keywords_.insert("orderby");
+  cs_keywords_.insert("partial");
+  cs_keywords_.insert("remove");
+  cs_keywords_.insert("select");
+  cs_keywords_.insert("set");
+  cs_keywords_.insert("value");
+  cs_keywords_.insert("var");
+  cs_keywords_.insert("where");
+  cs_keywords_.insert("yield");
 }
 
+// Helpers
 bool is_base_type(t_type* type) {
   t_type* t = get_underlying_type(type);
   return t->is_base_type();
-  }
+}
 
 t_type* get_underlying_type(t_type* type) {
   t_type* result = type;
@@ -866,8 +1103,6 @@ t_type* get_underlying_type(t_type* type) {
   }
   return result;
 }
-
-// Helpers
 
 string to_upper_case(string str) {
   string s(str);
