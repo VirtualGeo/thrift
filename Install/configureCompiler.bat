07e18a25ca7d6b83679a4621b8b6c53f17bb7fd1
Charly Mourglia <charly.mourglia@diginext.fr>
2018-10-31 16:35:57 +0100

    [FCT] Update compiler configuration

--- Install/configureCompiler.bat
+++ Install/configureCompiler.bat
@@ -2,36 +2,48 @@
 
 if %CURRENT_COMPILER% == Win32VC12 (
 	set GENERATOR="Visual Studio 12 2013"
+	set CS_CONFIGURATION=VC12_
+	set CS_PLATFORM=x86
 	set ZLIB_SUFFIX=vc12
 	goto :done
 )
 
 if %CURRENT_COMPILER% == Win64VC12 (
 	set GENERATOR="Visual Studio 12 2013 Win64"
+	set CS_CONFIGURATION=VC12_
+	set CS_PLATFORM=x64
 	set ZLIB_SUFFIX=vc12.x64
 	goto :done
 )
 
 if %CURRENT_COMPILER% == Win32VC14 (
 	set GENERATOR="Visual Studio 14 2015"
+	set CS_CONFIGURATION=VC14_
+	set CS_PLATFORM=x86
 	set ZLIB_SUFFIX=vc14
 	goto :done
 )
 
 if %CURRENT_COMPILER% == Win64VC14 (
 	set GENERATOR="Visual Studio 14 2015 Win64"
+	set CS_CONFIGURATION=VC14_
+	set CS_PLATFORM=x64
 	set ZLIB_SUFFIX=vc14.x64
 	goto :done
 )
 
 if %CURRENT_COMPILER% == Win32VC15 (
 	set GENERATOR="Visual Studio 15 2017"
+	set CS_CONFIGURATION=VC14_
+	set CS_PLATFORM=x86
 	set ZLIB_SUFFIX=vc14
 	goto :done
 )
 
 if %CURRENT_COMPILER% == Win64VC15 (
 	set GENERATOR="Visual Studio 15 2017 Win64"
+	set CS_CONFIGURATION=VC14_
+	set CS_PLATFORM=x64
 	set ZLIB_SUFFIX=vc14.x64
 	goto :done
 )
