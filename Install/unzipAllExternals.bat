b67302a83b8a759a8e33af479de9bcc549615a71
Charly Mourglia <charly.mourglia@diginext.fr>
2018-10-31 15:19:58 +0100

    [FCT/FIX] Use curl to download Bison

--- Install/unzipAllExternals.bat
+++ Install/unzipAllExternals.bat
@@ -8,7 +8,7 @@ if not exist %THRIFT_EXTERNAL% (
 
 set BISON_ZIP_URL=https://sourceforge.net/projects/winflexbison/files/win_flex_bison-latest.zip/download
 set BISON_ZIP=%THRIFT_EXTERNAL%\win_flex_bison-latest.zip
-bitsadmin /transfer BisonDL /download /priority normal %BISON_ZIP_URL% %BISON_ZIP%
+curl %BISON_ZIP_URL% --output %BISON_ZIP%
 
 7z.exe x %BISON_ZIP% -o%THRIFT_EXTERNAL%\win_flex_bison-latest -aoa
 
