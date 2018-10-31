8f7ca98ef6302d3df0fc0e3e6568f98d6276b4a5
Charly Mourglia <charly.mourglia@diginext.fr>
2018-10-31 16:33:52 +0100

    [FCT] Update build configs for csharp library

--- Install/doBuild.bat
+++ Install/doBuild.bat
@@ -8,11 +8,38 @@ echo Build debug...
 %BUILD_CMD% --config Debug %VISUAL_POSTFIX%
 echo Done. Installing...
 %BUILD_CMD%  --config Debug --target INSTALL %VISUAL_POSTFIX%
+if ERRORLEVEL 1 goto :error
 
 echo Build release...
 %BUILD_CMD% --config Release %VISUAL_POSTFIX%
 echo Done. Installing...
 %BUILD_CMD% --config Release --target INSTALL %VISUAL_POSTFIX%
-REM %BUILD_CMD% --config Build %VISUAL_POSTFIX%
+if ERRORLEVEL 1 goto :error
+
+call "%~dp0configureCompiler.bat"
+if ERRORLEVEL 1 goto :error
+
+rem Build C# Libraries
+pushd %~dp0..\lib\csharp\src
+
+msbuild Thrift.sln /p:Configuration=%CS_CONFIGURATION%Release /p:Platform=%CS_PLATFORM% /t:Clean,Build
+if ERRORLEVEL 1 (
+	popd
+	goto :error
+)
+
+msbuild Thrift.sln /p:Configuration=%CS_CONFIGURATION%Debug /p:Platform=%CS_PLATFORM% /t:Clean,Build
+if ERRORLEVEL 1 (
+	popd
+	goto :error
+)
+
+rem We will need to build java libraries aswell
 
 pause
+exit /b 0
+
+:error
+pause
+exit /b 42
+
