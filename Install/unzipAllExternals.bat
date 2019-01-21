703d9c2bf632bddc218f82765f7b657d2fcfd295
COMBES Bertrand <bcombes@diginext.local>
2019-01-21 12:10:30 +0100

    [FIX] Pas d'auth requise pour curl

--- Install/unzipAllExternals.bat
+++ Install/unzipAllExternals.bat
@@ -8,7 +8,7 @@ if not exist %THRIFT_EXTERNAL% (
 
 set BISON_ZIP_URL=https://sourceforge.net/projects/winflexbison/files/win_flex_bison-latest.zip
 set BISON_ZIP=%THRIFT_EXTERNAL%\win_flex_bison-latest.zip
-curl -L %BISON_ZIP_URL% > %BISON_ZIP%
+curl -k -L %BISON_ZIP_URL% > %BISON_ZIP%
 
 7z.exe x %BISON_ZIP% -o%THRIFT_EXTERNAL%\win_flex_bison-latest -aoa
 
